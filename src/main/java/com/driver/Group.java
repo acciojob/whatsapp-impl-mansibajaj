6576f4ec1372037e8a4c6d14377928cb491eb8fe
MANSI BAJAJ <121667209+mansibajaj@users.noreply.github.com>
2023-01-28 22:22:24 +0530

    Update Group.java

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -3,5 +3,26 @@ package com.driver;
 public class Group {
     private String name;
     private int numberOfParticipants;
+    //constuctor hai
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 
 }
